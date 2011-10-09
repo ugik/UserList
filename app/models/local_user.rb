0aa9b18f8821a3a56c93cf7475b495ac0143c5a7
GK <georgek@gmail.com>
2011-10-09 12:42:19 -0400

    first pass

--- app/models/local_user.rb
+++
@@ -1,2 +0,0 @@
-class LocalUser < ActiveRecord::Base
-end
--- test/unit/local_user_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LocalUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
