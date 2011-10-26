2f32fe1ec4940bc0fa64067793120516952eb653
GK <georgek@gmail.com>
2011-10-26 15:17:59 -0400

    kiosk mode

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -11,15 +11,32 @@ class AdminsController < ApplicationController
   end
 
   def show
-    @admin = Admin.find(params[:id])
-    @challenges = @admin.challenges
-
-    if session[:cached_id] != @admin.id           # if looking at un-cached challenge
-      expire_fragment('challenges_cache')   # expire cache
-      session[:cached_id] = @admin.id             # reset cookie
-      logger.debug("\n>>> Cached ID:"+ session[:cached_id].to_s)
+    if session[:shown_last] == nil
+      session[:shown_last] = 0
     end
 
+    if params[:id] == "0"
+      if session[:shown_last] == session[:shown_id].size    # cycle back
+        session[:shown_last] = 0
+      end
+
+      crumbs = Array.new(session[:shown_id])
+      session[:kiosk_mode] = true
+      logger.debug(">>> in Admin Controller CRUMB ITEM " + session[:shown_last].to_s)
+      logger.debug(">>> in Admin Controller CRUMBS " + crumbs.join(","))
+      logger.debug(">>> in Admin Controller SHOW " + crumbs[session[:shown_last]].to_s)
+
+      @admin = Admin.find(session[:shown_id][session[:shown_last]])    # show next id from crumbs
+      session[:shown_last] += 1
+      expire_fragment('challenges_cache')         # expire cache
+    else
+      session[:kiosk_mode] = false
+      @admin = Admin.find(params[:id])
+      handle_cache
+
+    end
+    @challenges = @admin.challenges
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @admin }
@@ -32,6 +49,8 @@ class AdminsController < ApplicationController
   end
 
   def update
+    session[:shown_id] = []                       # init array of shown id crumbs
+
     @admin = Admin.find(params[:id])
     if @admin.update_attributes(params[:admin])
       redirect_to @admin
@@ -41,4 +60,16 @@ class AdminsController < ApplicationController
     end
   end
 
+  def handle_cache
+    if session[:shown_id] == nil
+      session[:shown_id] = []                     # init array of shown id's
+    end
+    if session[:cached_id] != @admin.id           # if looking at un-cached challenge
+      expire_fragment('challenges_cache')         # expire cache
+      session[:cached_id] = @admin.id             # reset cookie
+      session[:shown_id] << @admin.id             # add to crumbs
+      logger.debug("\n>>> Cached ID:"+ session[:cached_id].to_s + " ...trail:" + session[:shown_id].join(","))
+    end
+
+  end
 end
